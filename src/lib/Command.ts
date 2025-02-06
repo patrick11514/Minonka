5e8b7c70af50d873d687a49435ae17ece2c8070e
Patrik Mintěl <ja@patrick115.eu>
2025-02-06 21:09:09 +0100

    Basic interface done

--- src/lib/Command.ts
+++ src/lib/Command.ts
@@ -1,4 +1,15 @@
-import { SlashCommandBuilder, type ChatInputCommandInteraction } from 'discord.js';
+import {
+    Locale,
+    SlashCommandAttachmentOption,
+    SlashCommandBooleanOption,
+    SlashCommandBuilder,
+    SlashCommandChannelOption,
+    SlashCommandMentionableOption,
+    SlashCommandRoleOption,
+    SlashCommandStringOption,
+    SlashCommandUserOption,
+    type ChatInputCommandInteraction
+} from 'discord.js';
 
 new SlashCommandBuilder().setNameLocalizations({
     'en-US': 'command',
@@ -12,8 +23,94 @@ export abstract class Command {
         this.slashCommand = new SlashCommandBuilder().setName(name).setDescription(description);
     }
 
-    public addOption(name: string, description: string, required: boolean) {
-        this.slashCommand.addStringOption((option) => option.setName(name).setDescription(description).setRequired(required));
+    public addLocalization(lang: Locale, name: string, description: string) {
+        this.slashCommand.setNameLocalization(lang, name);
+        this.slashCommand.setDescriptionLocalization(lang, description);
+    }
+
+    public addOption(
+        option: {
+            name: string;
+            localizedName?: Record<Locale, string>;
+            description: string;
+            localizedDescription?: Record<Locale, string>;
+            required: boolean;
+        } & (
+                | {
+                    type: 'STRING' | 'INTEGER' | 'NUMBER';
+                    min?: number;
+                    max?: number;
+                    choices?: { name: string; value: string }[];
+                }
+                | {
+                    type: 'BOOLEAN' | 'USER' | 'CHANNEL' | 'ROLE' | 'MENTIONABLE' | 'ATTACHMENT';
+                }
+            )
+    ) {
+        let optionBuilder:
+            | SlashCommandStringOption
+            | SlashCommandBooleanOption
+            | SlashCommandUserOption
+            | SlashCommandChannelOption
+            | SlashCommandRoleOption
+            | SlashCommandMentionableOption
+            | SlashCommandAttachmentOption;
+
+        if (option.type === 'STRING' || option.type === 'INTEGER' || option.type === 'NUMBER') {
+            switch (option.type) {
+                case 'STRING':
+                    optionBuilder = new SlashCommandStringOption();
+                    break;
+                case 'INTEGER':
+                    optionBuilder = new SlashCommandStringOption();
+                    break;
+                case 'NUMBER':
+                    optionBuilder = new SlashCommandStringOption();
+                    break;
+            }
+            if ('min' in option) {
+                optionBuilder.setMinLength(option.min as number);
+            }
+            if ('max' in option) {
+                optionBuilder.setMaxLength(option.max as number);
+            }
+            if ('choices' in option) {
+                optionBuilder.addChoices(option.choices as { name: string; value: string }[]);
+            }
+        } else {
+            switch (option.type) {
+                case 'BOOLEAN':
+                    optionBuilder = new SlashCommandBooleanOption();
+                    break;
+                case 'USER':
+                    optionBuilder = new SlashCommandUserOption();
+                    break;
+                case 'CHANNEL':
+                    optionBuilder = new SlashCommandChannelOption();
+                    break;
+                case 'ROLE':
+                    optionBuilder = new SlashCommandRoleOption();
+                    break;
+                case 'MENTIONABLE':
+                    optionBuilder = new SlashCommandMentionableOption();
+                    break;
+                case 'ATTACHMENT':
+                    optionBuilder = new SlashCommandAttachmentOption();
+                    break;
+            }
+        }
+
+        optionBuilder.setName(option.name).setDescription(option.description).setRequired(option.required);
+
+        if ('localizedName' in option) {
+            optionBuilder.setNameLocalizations(option.localizedName as Record<Locale, string>);
+        }
+
+        if ('localizedDescription' in option) {
+            optionBuilder.setDescriptionLocalizations(option.localizedDescription as Record<Locale, string>);
+        }
+
+        this.slashCommand.options.push(optionBuilder);
     }
 
     abstract handler(interaction: ChatInputCommandInteraction): void;
