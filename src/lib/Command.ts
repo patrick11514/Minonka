74d44f31cd7dbb05614bbd88da23a8efb5686de0
Patrik Mintěl <ja@patrick115.eu>
2025-02-08 23:25:34 +0100

    Add subcommand support

--- src/lib/Command.ts
+++ src/lib/Command.ts
@@ -1,17 +1,6 @@
-import {
-    APIApplicationCommandOptionChoice,
-    ClientEvents,
-    Locale,
-    SlashCommandAttachmentOption,
-    SlashCommandBooleanOption,
-    SlashCommandBuilder,
-    SlashCommandChannelOption,
-    SlashCommandMentionableOption,
-    SlashCommandRoleOption,
-    SlashCommandStringOption,
-    SlashCommandUserOption,
-    type ChatInputCommandInteraction
-} from 'discord.js';
+import { ClientEvents, Locale, SlashCommandBuilder, type ChatInputCommandInteraction } from 'discord.js';
+import { constructOption, OptionType } from './types';
+import { SubCommand } from './SubCommand';
 
 export abstract class Command {
     slashCommand: SlashCommandBuilder;
@@ -27,89 +16,12 @@ export abstract class Command {
         this.slashCommand.setDescriptionLocalization(lang, description);
     }
 
-    public addOption(
-        option: {
-            name: string;
-            localizedName?: Partial<Record<Locale, string>>;
-            description: string;
-            localizedDescription?: Partial<Record<Locale, string>>;
-            required: boolean;
-        } & (
-            | {
-                  type: 'STRING' | 'INTEGER' | 'NUMBER';
-                  min?: number;
-                  max?: number;
-                  choices?: APIApplicationCommandOptionChoice<string>[];
-              }
-            | {
-                  type: 'BOOLEAN' | 'USER' | 'CHANNEL' | 'ROLE' | 'MENTIONABLE' | 'ATTACHMENT';
-              }
-        )
-    ) {
-        let optionBuilder:
-            | SlashCommandStringOption
-            | SlashCommandBooleanOption
-            | SlashCommandUserOption
-            | SlashCommandChannelOption
-            | SlashCommandRoleOption
-            | SlashCommandMentionableOption
-            | SlashCommandAttachmentOption;
+    public addOption(option: OptionType) {
+        this.slashCommand.options.push(constructOption(option));
+    }
 
-        if (option.type === 'STRING' || option.type === 'INTEGER' || option.type === 'NUMBER') {
-            switch (option.type) {
-                case 'STRING':
-                    optionBuilder = new SlashCommandStringOption();
-                    break;
-                case 'INTEGER':
-                    optionBuilder = new SlashCommandStringOption();
-                    break;
-                case 'NUMBER':
-                    optionBuilder = new SlashCommandStringOption();
-                    break;
-            }
-            if ('min' in option) {
-                optionBuilder.setMinLength(option.min as number);
-            }
-            if ('max' in option) {
-                optionBuilder.setMaxLength(option.max as number);
-            }
-            if ('choices' in option) {
-                optionBuilder.addChoices(option.choices as APIApplicationCommandOptionChoice<string>[]);
-            }
-        } else {
-            switch (option.type) {
-                case 'BOOLEAN':
-                    optionBuilder = new SlashCommandBooleanOption();
-                    break;
-                case 'USER':
-                    optionBuilder = new SlashCommandUserOption();
-                    break;
-                case 'CHANNEL':
-                    optionBuilder = new SlashCommandChannelOption();
-                    break;
-                case 'ROLE':
-                    optionBuilder = new SlashCommandRoleOption();
-                    break;
-                case 'MENTIONABLE':
-                    optionBuilder = new SlashCommandMentionableOption();
-                    break;
-                case 'ATTACHMENT':
-                    optionBuilder = new SlashCommandAttachmentOption();
-                    break;
-            }
-        }
-
-        optionBuilder.setName(option.name).setDescription(option.description).setRequired(option.required);
-
-        if ('localizedName' in option) {
-            optionBuilder.setNameLocalizations(option.localizedName as Record<Locale, string>);
-        }
-
-        if ('localizedDescription' in option) {
-            optionBuilder.setDescriptionLocalizations(option.localizedDescription as Record<Locale, string>);
-        }
-
-        this.slashCommand.options.push(optionBuilder);
+    public addSubCommand(subCommand: SubCommand) {
+        this.slashCommand.addSubcommand(subCommand.subCommand);
     }
 
     public on<$Event extends keyof ClientEvents>(event: $Event, listener: (...args: ClientEvents[$Event]) => void) {
