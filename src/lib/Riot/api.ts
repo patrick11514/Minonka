477ee3ace306c721c6ccd6fc2000734be0a2ffcb
Patrik Mintěl <ja@patrick115.eu>
2026-01-17 19:48:01 +0100

    fix: Account routing value, for SEA servers

--- src/lib/Riot/api.ts
+++ src/lib/Riot/api.ts
@@ -1,46 +1,50 @@
 import { z } from 'zod';
 import { ApiSet } from './apiSet';
-import { rankType, Region, regions, tierType } from './types';
 import RiotAPI from './riotApi';
 import {
     AccountSchema,
     ChallengeSchema,
     ClashMemberSchema,
-    MatchSchema,
-    SummonerSchema,
     MasterySchema,
-    SpectatorSchema
+    MatchSchema,
+    SpectatorSchema,
+    SummonerSchema
 } from './schemes';
+import { rankType, Region, regions, tierType } from './types';
 
-const getBaseRoutingURL = (region: Region) => {
-    let prefix = '';
+const regionToServer = (region: Region) => {
     switch (region) {
         case 'EUN1':
         case 'EUW1':
         case 'ME1':
         case 'TR1':
         case 'RU':
-            prefix = 'EUROPE';
-            break;
+            return 'EUROPE';
         case 'NA1':
         case 'BR1':
         case 'LA1':
         case 'LA2':
-            prefix = 'AMERICAS';
-            break;
+            return 'AMERICAS';
         case 'KR':
         case 'JP1':
-            prefix = 'ASIA';
-            break;
+            return 'ASIA';
         case 'OC1':
         case 'SG2':
         case 'TW2':
         case 'VN2':
-            prefix = 'SEA';
-            break;
+            return 'SEA';
     }
+};
 
-    return `https://${prefix}.api.riotgames.com`;
+const getBaseRoutingURL = (region: Region) =>
+    `https://${regionToServer(region)}.api.riotgames.com`;
+
+const getAccountURL = (region: Region) => {
+    let server = regionToServer(region);
+    if (server === 'SEA') {
+        server = 'ASIA';
+    }
+    return `https://${server}.api.riotgames.com`;
 };
 
 const getBaseURL = (region: Region) => {
@@ -50,33 +54,33 @@ const getBaseURL = (region: Region) => {
 const RiotAPIStructure = {
     account: new ApiSet('/riot/account/v1/accounts', {
         name: (gameName: string, tagLine: string) => ({
-            regional: false,
+            type: 'account',
             endOfUrl: `/by-riot-id/${gameName}/${tagLine}`,
             schema: AccountSchema
         }),
         byPuuid: (puuid: string) => ({
-            regional: false,
+            type: 'account',
             endOfUrl: `/by-puuid/${puuid}`,
             schema: AccountSchema
         })
     }),
     summoner: new ApiSet('/lol/summoner/v4/summoners', {
         byPuuid: (puuid: string) => ({
-            regional: true,
+            type: 'regional',
             endOfUrl: `/by-puuid/${puuid}`,
             schema: SummonerSchema
         })
     }),
     challenges: new ApiSet('/lol/challenges/v1', {
         byPuuid: (puuid: string) => ({
-            regional: true,
+            type: 'regional',
             endOfUrl: `/player-data/${puuid}`,
             schema: ChallengeSchema
         })
     }),
     league: new ApiSet('/lol/league/v4', {
         byPuuid: (puuid: string) => ({
-            regional: true,
+            type: 'regional',
             endOfUrl: `/entries/by-puuid/${puuid}`,
             schema: z.array(
                 z.object({
@@ -110,7 +114,7 @@ const RiotAPIStructure = {
                 count: number;
             }>
         ) => ({
-            regional: false,
+            type: 'routing',
             endOfUrl: `/matches/by-puuid/${puuid}/ids?${new URLSearchParams({
                 ...{
                     start: '0',
@@ -126,14 +130,14 @@ const RiotAPIStructure = {
             schema: z.array(z.string())
         }),
         match: (matchId: string) => ({
-            regional: false,
+            type: 'routing',
             endOfUrl: `/matches/${matchId}`,
             schema: MatchSchema
         })
     }),
     clash: new ApiSet('/lol/clash/v1', {
         tournaments: () => ({
-            regional: true,
+            type: 'regional',
             endOfUrl: '/tournaments',
             schema: z.array(
                 z.object({
@@ -153,7 +157,7 @@ const RiotAPIStructure = {
             )
         }),
         players: (puuid: string) => ({
-            regional: true,
+            type: 'regional',
             endOfUrl: `/players/by-puuid/${puuid}`,
             schema: z.array(
                 ClashMemberSchema.extend({
@@ -162,7 +166,7 @@ const RiotAPIStructure = {
             )
         }),
         team: (teamId: string) => ({
-            regional: true,
+            type: 'regional',
             endOfUrl: `/teams/${teamId}`,
             schema: z.object({
                 id: z.string(),
@@ -178,32 +182,37 @@ const RiotAPIStructure = {
     }),
     mastery: new ApiSet('/lol/champion-mastery/v4', {
         byPuuid: (puuid: string) => ({
+            type: 'regional',
             endOfUrl: `/champion-masteries/by-puuid/${puuid}`,
-            regional: true,
             schema: z.array(MasterySchema)
         }),
         top: (puuid: string, count = 3) => ({
+            type: 'regional',
             endOfUrl: `/champion-masteries/by-puuid/${puuid}/top?count=${count}`,
-            regional: true,
             schema: z.array(MasterySchema)
         }),
         byChampionId: (puuid: string, championId: number) => ({
+            type: 'regional',
             endOfUrl: `/champion-masteries/by-puuid/${puuid}/by-champion/${championId}`,
-            regional: true,
             schema: MasterySchema
         })
     }),
     spectator: new ApiSet('/lol/spectator/v5', {
         byPuuid: (puuid: string) => ({
+            type: 'regional',
             endOfUrl: `/active-games/by-summoner/${puuid}`,
-            regional: true,
             schema: SpectatorSchema
         })
     })
 };
 
 const createForRegion = (region: Region) =>
-    RiotAPI(RiotAPIStructure, getBaseURL(region), getBaseRoutingURL(region));
+    RiotAPI(
+        RiotAPIStructure,
+        getBaseURL(region),
+        getBaseRoutingURL(region),
+        getAccountURL(region)
+    );
 
 export default Object.fromEntries(
     regions.map((region) => [region, createForRegion(region)])
