35cf7f5477b71fb840aed5972356348604f24d19
Patrik Mintěl <ja@patrick115.eu>
2026-04-26 12:20:14 +0200

    remove unused files in current deployment

--- script/restartWorkers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-WORKER_COUNT=3
-for i in $(seq 1 $WORKER_COUNT) ; do
-    sudo systemctl restart minonka_worker@${i}.service
-done
--- script/startWorker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export NVM_DIR="$HOME/.nvm"
-source "$NVM_DIR/nvm.sh"
-
-nvm use
-
-npm run start:worker
